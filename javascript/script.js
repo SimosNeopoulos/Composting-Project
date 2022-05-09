ee2e55a2eae360d195eab42b3f9178c853e6ee21
SimosNeopoulos <simneo22@gmail.com>
2022-05-09 19:56:30 +0300

    Checks regex validity for sign up

--- javascript/script.js
+++ javascript/script.js
@@ -28,20 +28,54 @@ function logInClick() {
 }
 
 function signUpClick() {
-    let username = document.getElementById("log-in-username").value;
-    let email = document.getElementById("log-in-email").value;
-    let address = document.getElementById("log-in-address").value;
-    let password = document.getElementById("log-in-password").value;
-    let phone = document.getElementById("log-in-phone").value;
-    /**
-     * TODO: Complete the function
-     */
+    let username = document.getElementById("sign-up-username").value;
+    let email = document.getElementById("sign-up-email").value;
+    let address = document.getElementById("sign-up-address").value;
+    let password = document.getElementById("sign-up-password").value;
+    let phone = document.getElementById("sign-up-tel").value;
+
+    if (!nameVerification(username)) {
+        alert("Your Username is not valid. Only alphabetic, numeric characters and '_' are acceptable.");
+        document.getElementById("sign-up-username").focus();
+        return;
+    }
+
+    if (!emailVerification(email)) {
+        alert("Email already exists");
+        document.getElementById("sign-up-email").focus();
+        return;
+    }
+
+    if (!passwordVerification(password)) {
+        alert("Password not valid");
+        document.getElementById("sign-up-password").focus();
+        return;
+    }
+
+    if (!phoneVerification(phone)) {
+        alert("Phone not valid");
+        document.getElementById("sign-up-tel").focus();
+    }
 }
 
-function signUpVerification() {
-    /**
-     * TODO: Write the function
-     */
+function nameVerification(username) {
+    let validUsername = /^[a-zA-Z0-9_]{4,16}$/.test(username);
+    let exists = false; /** TODO: check if username already exists in the database */
+    return validUsername && !exists;
+}
+
+function emailVerification(email) {
+    let validEmail = true; /** TODO: check if email already exists in the database */
+    return validEmail;
+}
+
+function passwordVerification(password) {
+    return /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/.test(password);
+}
+
+function phoneVerification(phone) {
+    let validPhone = true; /** TODO: check if email already exists in the database */
+    return validPhone;
 }
 
 function contactClick() {
