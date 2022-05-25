71d22330974154f95f2287f29d68abf143f08deb
SimosNeopoulos <simneo22@gmail.com>
2022-05-25 22:56:11 +0300

    Added unested queries for the posts etc

--- php/functions.php
+++ php/functions.php
@@ -3,6 +3,8 @@ session_start();
 include("../php/connect.php");
 include("../php/classes.php");
 
+/********** LOG IN / SIGN UP VERIFICATION **********/
+
 function nameExists($conn, $username) {
     $sql_query = "SELECT username FROM users WHERE username = '$username'";
     $result = mysqli_query($conn, $sql_query);
@@ -80,6 +82,8 @@ function logOut() {
     return true;
 }
 
+/****************************** *****************************/
+
 function getCurrentUserData(){
 
 }
@@ -123,5 +127,73 @@ function displayTelephone(){
         echo '';
     }
 }
+
+
+/************* POSTS / COMMENTS / TAGS *************/
+
+function deletePost($conn, $id) {
+    $sql_query = "DELETE FROM users WHERE id = '$id'";
+    mysqli_query($conn, $sql_query);
+    //$id = mysqli_insert_id($conn); Αν δεν περνει το id η συναρτηση
+    $sql_query = "DELETE FROM posts_has_tags WHERE posts_id = '$id'";
+    mysqli_query($conn, $sql_query);
+}
+
+function updatePost($conn, $id, $newBody) {
+    $sql_query = "UPDATE users SET body = '$newBody' WHERE id = '$id'";
+    mysqli_query($conn, $sql_query);
+}
+
+function getUserPosts($conn, $userId) {
+    $sql_query = "SELECT * FROM posts WHERE id_user = '$userId' ORDER BY post_date DESC";
+    $result = mysqli_query($conn, $sql_query);
+    $rows = mysqli_num_rows($result);
+    $data = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    mysqli_free_result($result);
+    if($rows < 1) {
+        echo "Δεν επιστρέφηκαν αποτελέσματα";
+        return;
+    }
+    return $data;
+}
+
+function getPostsFromArea($conn, $city) {
+    $sql_query = "SELECT posts.id, id_user, posts.body, posts.post_date 
+                  FROM users, posts 
+                  WHERE users.address = '$city' AND id_user = users.id 
+                  ORDER BY post_date DESC 
+                  LIMIT 5";
+    $result = mysqli_query($conn, $sql_query);
+    $rows = mysqli_num_rows($result);
+    $data = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    mysqli_free_result($result);
+    if($rows < 1) {
+        echo "Δεν επιστρέφηκαν αποτελέσματα";
+        return;
+    }
+    return $data;
+}
+
+function getPostsWithTag($conn, $tags) {
+    $where_query = "( ";
+    $numElements = count($tags);
+    for($i=0; $i<$numElements; $i++) {
+        $where_query .= "tag_name = " . $tags[$i];
+        if($i+1 !== $numElements) {
+            $where_query .= " OR ";
+        }
+    }
+    $where_query .= ")";
+    $sql_query = "SELECT posts.id, posts.id_user, posts.body, posts.post_date 
+                  FROM posts INNNER JOIN posts_has_tags
+                  ON posts.id = posts_id
+                  INNER JOIN tags
+                  ON tags.id = tags_id
+                  WHERE $where_query
+                  ORDER BY post_date DESC 
+                  LIMIT 5";
+}
+
+/************************  *************************/
 ?>
 
