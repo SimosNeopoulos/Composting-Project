b8cd42db1a67739d3488536a5590be8477fec2fb
SimosNeopoulos <simneo22@gmail.com>
2022-05-29 20:46:51 +0300

    Added footer functionality

---
+++ php/footer.php
@@ -0,0 +1,15 @@
+<footer>
+    <div class="footer-content">
+        <h3><b>Composting</b></h3>
+        <p>You can also find us on our social media!</p>
+        <ul class="socials">
+            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+            <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+        </ul>
+    </div>
+    <div class="footer-bottom">
+        <p>copyright &copy;2022 composting</p>
+    </div>
+</footer>
